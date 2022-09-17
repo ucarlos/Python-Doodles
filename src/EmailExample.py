682bf2e7b6866444fea40bc5fa43e67aa9bbd4d4
Ulysses Carlos <ulysses_carlos@protonmail.com>
2022-09-17 11:59:14 -0400

    Initial version of EmailExample.py

---
+++ src/EmailExample.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -------------------------------------------------------------------------------
+# Created by Ulysses Carlos on 08/06/2022 at 08:26 PM
+#
+# EmailExample.py
+#
+# -------------------------------------------------------------------------------
+
+from smtplib import SMTP
+
+body_message =
+"""
+The smtplib module defines an SMTP client session object that can be used to send mail to any internet machine with an SMTP or ESMTP listener
+daemon. For details of SMTP and ESMTP operation, consult RFC 821 (Simple Mail Transfer Protocol) and RFC 1869 (SMTP Service Extensions).
+"""
+
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

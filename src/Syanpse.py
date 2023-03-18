3eaff680bd9b4577705726975d8d5c33d7dc3694
Ulysses Carlos <ulysses_carlos@protonmail.com>
2023-03-17 23:01:53 -0400

    Syanpse.py - Each Argument function now works

--- src/Syanpse.py
+++ src/Syanpse.py
@@ -8,20 +8,27 @@
 from pathlib import Path
 import requests
 import logging
+import json
 import argparse
+from time import time
+from datetime import datetime
+from subprocess import run
+from shlex import split
+
 
 ACCESS_TOKEN = ""
 SYNAPSE_USER = "matrix"
 
 ROOT = Path(Path.cwd().root)
-SYNAPSE_EXECUTABLE = ROOT / SYNAPSE_USER / ".local" / "bin" / "synctl"
-SYNAPSE_MAIN_DIRECTORY = ROOT / SYNAPSE_USER / "Synapse"
-BASE_URL = "http://localhost:8080"
+SYNAPSE_EXECUTABLE = ROOT / "home" / SYNAPSE_USER / ".local" / "bin" / "synctl"
+SYNAPSE_MAIN_DIRECTORY = ROOT / "home" / SYNAPSE_USER / "Synapse"
+YAML_PATH = SYNAPSE_MAIN_DIRECTORY / "config" / "homeserver.yaml"
+BASE_URL = "http://localhost:8008"
 
-DEBUG_MODE = False
+DEBUG_MODE = True
 
 
-def execute_http_request(http_method_callback, url_path, data_parameters, url_parameters):
+def execute_http_request(http_method, url_path, data_parameters={}, url_parameters={}):
     """Execute a HTTP Request using the requests library.
 
     :param http_mthod_callback:
@@ -32,12 +39,19 @@ def execute_http_request(http_method_callback, url_path, data_parameters, url_pa
     """
     logging.debug("execute_http_request: Attempting to create a HTTP request with the following information:")
     logging.debug(f"\nURL Link: {url_path}\nData Options: {data_parameters}\nURL Parameters: {url_parameters}")
+
+    # Make sure to turn the data_parameters into JSON:
+    json_data = json.dumps(data_parameters)
+
     try:
-        request = http_method_callback(url_path, data=data_parameters, url_aram=url_parameters)
+        request = requests.request(http_method, url_path, data=json_data, params=url_parameters)
         request.raise_for_status()
-    except request.HTTPError:
-        logging.error("Error: HTTP POST request failed!")
-        print("Error: The HTTP POST request failed!")
+    except requests.HTTPError:
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            logging.error(f"Error: HTTP {http_method} request failed!")
+            logging.error(f"Here's the JSON Response: {request.json()}")
+        else:
+            print(f"Error: The HTTP {http_method} Request failed!")
         exit(1)
 
     return dict(request.json())
@@ -45,64 +59,123 @@ def execute_http_request(http_method_callback, url_path, data_parameters, url_pa
 
 def generate_registration_token():
     """Generate a registration Token."""
-    valid_token_time = "10min"
+    # Set the registration token to expire within 10 minutes:
+    valid_token_time_in_min = 10
+    unix_expiry_time = int(time()) + valid_token_time_in_min * 60
 
     data_options = {
         "access_token": ACCESS_TOKEN,
         "uses_allowed": 1,
-        "expiry_time": valid_token_time,
+        "expiry_time": unix_expiry_time * 1000,
         "length": 48
     }
 
     parameters = {"access_token": ACCESS_TOKEN}
 
-    expiration_date = ""
+    expiration_date = datetime.fromtimestamp(unix_expiry_time).strftime("%A, %B %d %Y %H:%M:%S")
     url_path = f"{BASE_URL}/_synapse/admin/v1/registration_tokens/new"
-    response = execute_http_request(requests.post, url_path, data_options, parameters)
 
-    # logging.debug("Attempting to create a HTTP POST request with the following information:")
-    # logging.debug(f"\nURL Link: {url_path}\nData Options: {data_options}\nParameters: {parameters}")
-    # try:
-    #     request = requests.post(url_path, data=data_options, params=parameters)
-    #     request.raise_for_status()
-    # except request.HTTPError:
-    #     logging.error("Error: HTTP POST request failed!")
-    #     print("Error: The HTTP POST request failed!")
+    response = execute_http_request("post", url_path, data_options, parameters)
 
-    if not response['registration_tokens']:
-        raise KeyError("Error: Could not find the 'registration_tokens' key in the response dictionary.")
+    if not response['token']:
+        raise KeyError("Error: Could not find the 'token' key in the response dictionary.")
     else:
-        token_response = response['registration_tokens']
-        token = token_response['token'] if token_response['token'] else ""
+        token = response['token'] if response['token'] else ""
         # expiry_time  = token_response['expiry_time'] if token_response['expiry_time'] else ""
 
         print(f"Your registration Token is {token}")
         print(f"It is valid until {expiration_date}")
 
 
-def delete_registration_token(token=""):
+def delete_registration_token(token):
     """Delete a specific registration Token."""
-    pass
+    data_options = {
+        "access_token": ACCESS_TOKEN
+    }
+
+    parameters = {
+        "access_token": ACCESS_TOKEN,
+    }
+
+    url_path = f"{BASE_URL}/_synapse/admin/v1/registration_tokens/{token}"
+
+    response = execute_http_request("delete", url_path, data_options, parameters)
+
+    if not response:
+        print(f"Token {token} has been deleted succesfully.")
+    else:
+        print(f"An Error has occurred while deleting token {token}:")
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            logging.debug(f"JSON response: {str(response)}")
 
 
 def get_all_registration_tokens():
     """Retrieve all registration tokens."""
-    pass
+    data_options = {
+        "access_token": ACCESS_TOKEN
+    }
+
+    parameters = {
+        "access_token": ACCESS_TOKEN
+    }
+
+    url_path = f"{BASE_URL}/_synapse/admin/v1/registration_tokens"
+
+    response = execute_http_request("get", url_path, data_options, parameters)
+
+    logging.debug(f"Response: {str(response)}")
+
+    return response['registration_tokens']
+
+
+def print_all_registration_tokens():
+    """Print each existing registration token."""
+    token_list = get_all_registration_tokens()
+
+    if not token_list:
+        print("Error: No Registration_tokens were found.")
+    else:
+        print(f"{len(token_list)} token(s) found:")
+        for token in token_list:
+            print("-" * 80)
+            print(f"Token Name  : {token['token']}")
+            print(f"Uses Allowed: {token['uses_allowed']}")
+            pending_status = "No" if not token['pending'] else "Yes"
+            completed_status = "No" if not token['completed'] else "Yes"
+
+            expiration_date = "n/a"
+            if token['expiry_time'] > 0:
+                expiration_date = datetime.fromtimestamp(token['expiry_time'] / 1000).strftime("%A, %B %d %Y %H:%M:%S")
+
+            print(f"Pending?    : {pending_status}")
+            print(f"Completed?  : {completed_status}")
+            print(f"Expires on  : {expiration_date}")
+            print("-" * 80 + "\n")
 
 
 def delete_all_registration_tokens():
     """Delete all registration tokens."""
-    pass
+    token_list = get_all_registration_tokens()
+
+    if len(token_list):
+        print("Deleting all registration tokens:")
+        for token in token_list:
+            delete_registration_token(token['token'])
+        print("Complete!")
+    else:
+        print("Error: No Registration tokens found.")
 
 
 def start_synapse():
     """Start up the Synapse server."""
-    pass
+    command = f"sudo -u {SYNAPSE_USER} {str(SYNAPSE_EXECUTABLE)} start {str(YAML_PATH)}"
+    run(split(command))
 
 
 def stop_synapse():
     """Shutdown the Synapse server."""
-    pass
+    command = f"sudo -u {SYNAPSE_USER} {str(SYNAPSE_EXECUTABLE)} stop {str(YAML_PATH)}"
+    run(split(command))
 
 
 def argparse_condition(key, value):
@@ -125,9 +198,11 @@ def is_argparse_list_valid(argparse_list):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     if DEBUG_MODE:
-        ACCESS_TOKEN = "syt_dWx5c3Nlcw_FPpGhLTtdxmkMTJpBZuG_0rPk1N"
-        generate_registration_token()
+        ACCESS_TOKEN = "syt_dWx5c3Nlcw_OOPINcqtvkaeHnfGJcJB_3CQSYz"
+        # start_synapse()
+        exit(0)
 
     logging.basicConfig(level=logging.DEBUG)
     parser = argparse.ArgumentParser()

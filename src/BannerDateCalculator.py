e5b600e9bf85a53edc4904e9b9518991841da56b
Ulysses Carlos <ulysses_carlos@protonmail.com>
2024-05-18 14:24:46 -0400

    BannerDateCalculator.py: Fixed some formatted and added additional date

--- src/BannerDateCalculator.py
+++ src/BannerDateCalculator.py
@@ -9,10 +9,13 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
+ZFILL_LENGTH = 2
+
 if __name__ == "__main__":
     current_date = datetime.today()
     current_birthday = datetime(current_date.year, 2, 15, 22, 5)
     new_years_date = datetime(current_date.year, 1, 1, 0, 0)
+    departure_date = datetime(2022, 11, 1, 1)
 
     if current_birthday > current_date:
         # Decrement the birthday year
@@ -21,27 +24,40 @@ if __name__ == "__main__":
     # Now, provide the difference for the months:
     relative_birthday_difference: relativedelta = relativedelta(current_date, current_birthday)
     relative_new_years_date_difference: relativedelta = relativedelta(current_date, new_years_date)
+    relative_departure_date_difference: relativedelta = relativedelta(current_date, departure_date)
 
     # Now use normal delta
     birthday_difference = current_date - current_birthday
     new_years_date_difference = current_date - new_years_date
+    departure_date_difference = current_date - relative_birthday_difference
 
-    birthday_difference_string = (f"{relative_birthday_difference.months} Months "
-                                  f"{str(relative_birthday_difference.days).zfill(2)} "
-                                  f"Days {str(relative_birthday_difference.hours).zfill(2)}:"
-                                  f"{str(relative_birthday_difference.minutes).zfill(2)}:"
-                                  f"{str(relative_birthday_difference.seconds).zfill(2)} "
+    birthday_difference_string = (f"{str(relative_birthday_difference.months).zfill(ZFILL_LENGTH)} Months "
+                                  f"{str(relative_birthday_difference.days).zfill(ZFILL_LENGTH)} "
+                                  f"Days {str(relative_birthday_difference.hours).zfill(ZFILL_LENGTH)}:"
+                                  f"{str(relative_birthday_difference.minutes).zfill(ZFILL_LENGTH)}:"
+                                  f"{str(relative_birthday_difference.seconds).zfill(ZFILL_LENGTH)} "
                                   f"({birthday_difference})")
 
-    current_year_to_date_string = (f"{relative_new_years_date_difference.months} Months "
-                                   f"{str(relative_new_years_date_difference.days).zfill(2)} Days "
-                                   f"{str(relative_new_years_date_difference.hours).zfill(2)}:"
-                                   f"{str(relative_new_years_date_difference.minutes).zfill(2)}:"
-                                   f"{str(relative_new_years_date_difference.seconds).zfill(2)} "
+    current_year_to_date_string = (f"{str(relative_new_years_date_difference.months).zfill(ZFILL_LENGTH)} Months "
+                                   f"{str(relative_new_years_date_difference.days).zfill(ZFILL_LENGTH)} Days "
+                                   f"{str(relative_new_years_date_difference.hours).zfill(ZFILL_LENGTH)}:"
+                                   f"{str(relative_new_years_date_difference.minutes).zfill(ZFILL_LENGTH)}:"
+                                   f"{str(relative_new_years_date_difference.seconds).zfill(ZFILL_LENGTH)} "
                                    f"({new_years_date_difference})")
+
+    departure_date_string = (f"{relative_departure_date_difference.years} Years "
+                             f"{str(relative_departure_date_difference.months).zfill(ZFILL_LENGTH)} Months "
+                             f"{str(relative_departure_date_difference.days).zfill(ZFILL_LENGTH)} Days "
+                             f"{str(relative_departure_date_difference.hours).zfill(ZFILL_LENGTH)}:"
+                             f"{str(relative_departure_date_difference.minutes).zfill(ZFILL_LENGTH)}:"
+                             f"{str(relative_departure_date_difference.seconds).zfill(ZFILL_LENGTH)} "
+                             f"({departure_date_difference})")
 
     formatted_new_years_string = new_years_date.strftime("%Y-%m-%d")
     formatted_birthday_string = current_birthday.strftime("%Y-%m-%d")
+    formatted_departure_string = departure_date.strftime("%Y-%m-%d")
 
-    print(f"Time since New Years Day ({formatted_new_years_string}): {current_year_to_date_string}")
-    print(f"Time since last Birthday ({formatted_birthday_string}): {birthday_difference_string}")
+    print(f"Days since the departure ({formatted_departure_string}): {departure_date_string}\n")
+
+    print(f"Days since New Years Day ({formatted_new_years_string}): {current_year_to_date_string}")
+    print(f"Days since last Birthday ({formatted_birthday_string}): {birthday_difference_string}")

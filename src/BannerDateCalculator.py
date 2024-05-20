3547f047785c5842f3a396c3b3c3aee0c8330154
Ulysses Carlos <ulysses_carlos@protonmail.com>
2024-05-20 14:10:07 -0400

    BannerDateCalculator.py: Additional Changes

--- src/BannerDateCalculator.py
+++ src/BannerDateCalculator.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     current_birthday = datetime(current_date.year, 2, 15, 22, 5)
     new_years_date = datetime(current_date.year, 1, 1, 0, 0)
     departure_date = datetime(2022, 11, 1, 1)
+    graduation_date = datetime(2020, 12, 18, 14, 7)
 
     if current_birthday > current_date:
         # Decrement the birthday year
@@ -25,11 +26,13 @@ if __name__ == "__main__":
     relative_birthday_difference: relativedelta = relativedelta(current_date, current_birthday)
     relative_new_years_date_difference: relativedelta = relativedelta(current_date, new_years_date)
     relative_departure_date_difference: relativedelta = relativedelta(current_date, departure_date)
+    relative_graduation_date_difference: relativedelta = relativedelta(current_date, graduation_date)
 
     # Now use normal delta
     birthday_difference = current_date - current_birthday
     new_years_date_difference = current_date - new_years_date
-    departure_date_difference = current_date - relative_birthday_difference
+    departure_date_difference = current_date - departure_date
+    graudation_departure_date_difference = current_date - graduation_date
 
     birthday_difference_string = (f"{str(relative_birthday_difference.months).zfill(ZFILL_LENGTH)} Months "
                                   f"{str(relative_birthday_difference.days).zfill(ZFILL_LENGTH)} "
@@ -53,11 +56,21 @@ if __name__ == "__main__":
                              f"{str(relative_departure_date_difference.seconds).zfill(ZFILL_LENGTH)} "
                              f"({departure_date_difference})")
 
+    graduation_departure_date_string = (f"{relative_graduation_date_difference.years} Years "
+                                        f"{str(relative_graduation_date_difference.months).zfill(ZFILL_LENGTH)} Months "
+                                        f"{str(relative_graduation_date_difference.days).zfill(ZFILL_LENGTH)} Days "
+                                        f"{str(relative_graduation_date_difference.hours).zfill(ZFILL_LENGTH)}:"
+                                        f"{str(relative_graduation_date_difference.minutes).zfill(ZFILL_LENGTH)}:"
+                                        f"{str(relative_graduation_date_difference.seconds).zfill(ZFILL_LENGTH)} "
+                                        f"({graudation_departure_date_difference})")
+
     formatted_new_years_string = new_years_date.strftime("%Y-%m-%d")
     formatted_birthday_string = current_birthday.strftime("%Y-%m-%d")
     formatted_departure_string = departure_date.strftime("%Y-%m-%d")
+    formatted_graduation_string = graduation_date.strftime("%Y-%m-%d")
 
-    print(f"Days since the departure ({formatted_departure_string}): {departure_date_string}\n")
+    print(f"Days since College Graduation ({formatted_graduation_string}): {graduation_departure_date_string}")
+    print(f"Days since Dad's Departure    ({formatted_departure_string}): {departure_date_string}\n")
 
     print(f"Days since New Years Day ({formatted_new_years_string}): {current_year_to_date_string}")
-    print(f"Days since last Birthday ({formatted_birthday_string}): {birthday_difference_string}")
+    print(f"Days since Last Birthday ({formatted_birthday_string}): {birthday_difference_string}")
